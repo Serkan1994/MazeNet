8d769195c21ce7c7ecda78c7611914b6531aded9
Tim Watzlawik <tim.watzlawik@alumni.fh-aachen.de>
2017-06-22 10:04:31 +0200

    Added Switch for MazeCom message types.

--- src/Client.java
+++ src/Client.java
@@ -1,51 +1,67 @@
+
 /**
  * Created by sesc on 20.06.17.
  */
 //package de.fhac.rn.xml;
 
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
 import javax.xml.bind.JAXBException;
 
+import generated.AwaitMoveMessageType;
+import generated.LoginReplyMessageType;
+import generated.MazeCom;
+
 //import mypackage.EchoMessage;
 
 public class Client {
 
-    public static void main(String[] args) throws UnknownHostException, IOException, JAXBException {
-        System.out.println("client starts");
-        Socket socket = new Socket("localhost", 5123);
-        if(socket.isConnected()) {
-            System.out.println("success");
-        }
+	private int id;
 
-        UTFOutputStream dataOutputStream = new UTFOutputStream(socket.getOutputStream());
-        
-        String msg = XMLHandler.getInstance().getMazeCom();
-       
-        System.out.println(msg);
-        
-        dataOutputStream.writeUTF8(msg);
-        dataOutputStream.flush();
-        
-//        XMLSerialisation serializer = new XMLSerialisation(socket.getLocalAddress().getHostName());
+	private static boolean VERBOSE = true;
 
-//        Scanner scanner = new Scanner(System.in);
-//        DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
-//        DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
-//        while (scanner.hasNextLine()) {
-//            EchoMessage message = serializer.getNewMessage();
-//            message.setContent(scanner.nextLine());
-//            dataOutputStream.writeUTF(serializer.messageToXMLString(message));
-//            EchoMessage answer = serializer.xmlStringToMessage(dataInputStream.readUTF());
-//
-//            System.out.println(answer.getSender() + ": " + answer.getContent());
-//        }
-//        scanner.close();
-        socket.close();
-    }
-    
+	public static void main(String[] args) throws UnknownHostException, IOException, JAXBException {
+		new Client();
+	}
 
+	public Client() throws UnknownHostException, IOException, JAXBException {
+		System.out.println("client starts");
+		Socket socket = new Socket("localhost", 5123);
+		if (socket.isConnected()) {
+			System.out.println("success");
+		}
+
+		UTFOutputStream outputStream = new UTFOutputStream(socket.getOutputStream());
+
+		String msg = XMLHandler.getInstance().createLoginMessage();
+
+		outputStream.writeUTF8(msg);
+		outputStream.flush();
+
+		UTFInputStream inStream = new UTFInputStream(socket.getInputStream());
+		
+
+		while (true) {
+			String answer = inStream.readUTF8();
+			MazeCom mazeCom = XMLHandler.getInstance().getMessage(answer);
+			switch (mazeCom.getMcType()) {
+			case LOGINREPLY:
+				LoginReplyMessageType loginReplyMsg = mazeCom.getLoginReplyMessage();
+				this.id = loginReplyMsg.getNewID();
+				if (VERBOSE) {
+					System.out.println("Login Reply Message received.");
+					System.out.println("ID: " + this.id);
+				}
+				break;
+			case AWAITMOVE:
+				AwaitMoveMessageType awaitMoveMsg = mazeCom.getAwaitMoveMessage();
+				if (VERBOSE) {
+					System.out.println("Await Move Message received.");
+				}
+			}
+		}
+
+	}
 }
