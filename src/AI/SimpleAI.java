e7b58d88e97d238ce37fe902672521fd25fc6110
sesc <schaffrath.sebastian@gmail.com>
2017-06-22 11:03:25 +0200

    added simple Client connection

--- src/AI/SimpleAI.java
+++ src/AI/SimpleAI.java
@@ -1,38 +1,47 @@
 package AI;
+
 import java.util.List;
 
-import generated.AwaitMoveMessageType;
-import generated.BoardType;
-import generated.MoveMessageType;
-import generated.PositionType;
-import generated.TreasureType;
-import generated.TreasuresToGoType;
+import generated.*;
 import util.XMLHandler;
 
+import static util.XMLHandler.*;
+
 public class SimpleAI implements AI {
-	
-	public SimpleAI() {}
-	
-	public MoveMessageType getNextMove(AwaitMoveMessageType ammt) {
-		BoardType bt = ammt.getBoard();
-		List<TreasuresToGoType> ttgt = ammt.getTreasuresToGo();
-		List<TreasureType> ftt = ammt.getFoundTreasures();
-		TreasureType tt = ammt.getTreasure();
-		
-		MoveMessageType mmt = XMLHandler.getInstance().createMoveMessage();
-		PositionType pt = XMLHandler.getInstance().createPosition();
-		mmt.setShiftCard(bt.getShiftCard());
-		pt.setCol(1);
-		pt.setRow(1);
-		mmt.setShiftPosition(pt);
-		pt.setCol(0);
-		pt.setRow(1);
-		mmt.setNewPinPos(pt);
-		return mmt;
-		
-		
-		
-		
-		
-	}
+
+    int playerID;
+
+    public SimpleAI(int id) {
+        this.playerID=id;
+    }
+
+    public MoveMessageType getNextMove(AwaitMoveMessageType ammt) {
+        BoardType bt = ammt.getBoard();
+        List<TreasuresToGoType> ttgt = ammt.getTreasuresToGo();
+        List<TreasureType> ftt = ammt.getFoundTreasures();
+        TreasureType tt = ammt.getTreasure();
+
+
+//        MoveMessageType mmt = getInstance().createMoveMessage();
+//        for (BoardType.Row row : bt.getRow()) {
+//            for (CardType ct : row.getCol()) {
+//                if (ct.getPin().getPlayerID().contains(playerID)){
+                    
+//                }
+//            }
+//        }
+        MoveMessageType mmt = XMLHandler.getInstance().createMoveMessage();
+        PositionType pt = XMLHandler.getInstance().createPosition();
+        mmt.setShiftCard(bt.getShiftCard());
+        pt.setCol(1);
+        pt.setRow(1);
+        mmt.setShiftPosition(pt);
+        pt.setCol(0);
+        pt.setRow(1);
+        mmt.setNewPinPos(pt);
+        return mmt;
+
+
+
+    }
 }
